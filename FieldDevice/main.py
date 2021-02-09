0cb10e471df1a03256fa4d3c538e7c19ef24a422
MBavelock <46979268+MBavelock@users.noreply.github.com>
2021-02-09 16:47:07 -0500

    Update main.py

--- FieldDevice/main.py
+++ FieldDevice/main.py
@@ -1,2 +1,35 @@
-*main.py will run on startup of system*
-*will run and coordinate all processes within field device*
+#*main.py will run on startup of system*
+#*will run and coordinate all processes within field device*
+
+
+# import libraries
+
+# Start Main
+def main():
+  # Init Stuff
+    # Check connection with home (radio)
+    # Check (maybe) power
+    # Check camera status
+    # load model and perform inital inference
+    # Check (maybe) deterrents
+  while true:
+    # PIR monitoring (either parallel script or poll in main)
+      # if PIR hit
+        # Start Record entry - Time/Date
+        # Sample monitored space (either parallel script take main space)
+        # for loop i = 5?
+          # take picture
+          # inference picture
+        # determine possitive classification (either parallel script take main space)
+        # if possitive classification
+          # 
+        # else
+          # Leave current PIR hit
+          # Clear Record Entry - Note hit but not classify?
+    
+    
+    
+  
+
+if __name__ == __main__:
+  main()
