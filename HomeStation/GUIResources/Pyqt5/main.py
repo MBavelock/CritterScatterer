e00275bf0998fcb6117459217afb87806f83aea6
acunningham3 <43209704+acunningham3@users.noreply.github.com>
2021-03-06 22:56:10 -0500

    Comments, organization
    Tried for mad long to make class objects to clean things but I got no where. It's something that I will work on as a stretch goal once this is all done.

--- HomeStation/GUIResources/Pyqt5/main.py
+++ HomeStation/GUIResources/Pyqt5/main.py
@@ -1,49 +1,75 @@
+#-------------------------------------------------------------------------#
+#University of Massachusetts Darmouth, College of Engineering             #
+#Class:     ECE 457/458                                                   #
+#Team:      ECE-3 Critter Scatterer                                       #
+#Author:    Adam Cunningham                                               #
+#Purpose:   This main python program holds all the information for the GUI#
+#           to function properly.                                         #
+#Credit:    z3ntu - QtWaitingSpinner                                      #
+#-------------------------------------------------------------------------#
+
 import sys
 import urllib.request
 from PyQt5.uic import loadUi
 from PyQt5 import QtWebEngineWidgets, QtWidgets, QtCore
+from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import QMainWindow, QDialog, QApplication, QPushButton, QProgressBar
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QColor, QIcon
 from waitingspinnerwidget import QtWaitingSpinner
+from functools import partial
 
+#Main window
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        loadUi("UIs/main.ui", self)
-        self.Options.clicked.connect(self.SwitchOptions)    #When options button is clicked, call SwitchOptions
-        self.Events.clicked.connect(self.SwitchEvents)      #When events button is clicked, call SwitchEvents
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        loadUi("UIs/main.ui", self) #Load the main UI created with Qt Designer
+        self.ButtonIcons()
 
-    def SwitchOptions(self):
-        widget.setCurrentIndex(1)   #Options menu is index 1 in stack
+        #When options is clicked, switch to the options window
+        self.Options.clicked.connect(partial(WindowSwitch.Switch, name = 'Options'))
+        #When events is clicked, switch to the events window
+        self.Events.clicked.connect(partial(WindowSwitch.Switch, name = 'Events'))
 
-    def SwitchEvents(self):
-        widget.setCurrentIndex(2)   #Events menu is index 2 in stack
+    def ButtonIcons(self): 
+        #Set icon and size for options button
+        self.Options.setIcon(QIcon("Images/Options Button.svg"))
+        self.Options.setIconSize(QSize(170,170))
 
+        #Set icon and size for events button
+        self.Events.setIcon(QIcon("Images/Events Button.svg"))
+        self.Events.setIconSize(QSize(170,170))
 
-class OptionsMenu(QDialog):
-    def __init__(self):
-        super(OptionsMenu, self).__init__()
-        loadUi("UIs/options.ui", self)
-        self.goBack.clicked.connect(self.GoBack)            #When goBack button is clicked, call GoBack
-        self.optEvents.clicked.connect(self.OptionsToEvents)    #When Events button is clicked, switch to Events dialog
+        #Set icon and size for on/off button
+        self.OnOff.setIcon(QIcon("Images/Off Button.svg"))
+        self.OnOff.setIconSize(QSize(170,170))
 
-    def OptionsToEvents(self):
-        widget.setCurrentIndex(2)   #Events menu is index 2 in stack
+#Options window
+class Options(QDialog):
+    def __init__(self, parent=None):
+        super(Options, self).__init__(parent)
+        loadUi("UIs/options.ui", self)  #Load the options UI created with Qt Designer
 
-    def GoBack(self):
-        widget.setCurrentIndex(0)   #Mainwindow is index 0 in stack
+        #When go back is clicked, switch to the main menu
+        self.goBack.clicked.connect(partial(WindowSwitch.Switch, name = 'Main'))
+        #When events is clicked, switch to the events menu
+        self.optEvents.clicked.connect(partial(WindowSwitch.Switch, name = 'Events'))
 
+#Event window
+class Events(QDialog):
+    def __init__(self, parent=None):
+        super(Events, self).__init__(parent)
+        loadUi("UIs/events.ui", self)   #Load the events UI created with Qt Designer
 
-class EventsMenu(QDialog):
-    def __init__(self):
-        super(EventsMenu, self).__init__()
-        loadUi("UIs/events.ui", self)
-        self.goBack.clicked.connect(self.GoBack)            #When goBack button is clicked, call GoBack
-        self.evntOptions.clicked.connect(self.EventsToOptions)    #When Options button is clicked, switch to Options dialog
+        #When go back is clicked, switch to the main menu
+        self.goBack.clicked.connect(partial(WindowSwitch.Switch, name = 'Main'))
+        #When options is clicked, switch to the options window
+        self.evntOptions.clicked.connect(partial(WindowSwitch.Switch, name = 'Options'))
+
         self.SpinnerInit()
-        self.RefreshGrid.addWidget(self.spinner, 0, 1, 1, 2)  #Add spinner to column 1 of the RefreshGrid
+        self.RefreshLayout.addWidget(self.spinner)  #Add spinner to column 1 of the RefreshGrid
         self.refresh.clicked.connect(self.Download)    #If refresh button is clicked, start downloader
 
+    #Method to initialize the waiting spinner
     def SpinnerInit(self):
         self.spinner = QtWaitingSpinner(self, centerOnParent=False)   #Create new spinner
         self.spinner.setRoundness(60)
@@ -57,18 +83,14 @@ class EventsMenu(QDialog):
         self.spinner.setColor(QColor(7, 47, 157))
         self.spinner.move(30,450)
 
+    #Toggle the status of the waiting spinner (on or off)
     def ToggleSpinner(self):
         if self.spinner.isSpinning():
             self.spinner.stop()
         else:
             self.spinner.start()
 
-    def EventsToOptions(self):
-        widget.setCurrentIndex(1)   #Options menu is index 2 in stack
-
-    def GoBack(self):
-        widget.setCurrentIndex(0)   #Mainwindow is index 0 in stack
-
+    #Calculate download progress and show spinner during that time
     def DownProgress(self, blocknum, blocksize, totalsize): 
         data = blocknum * blocksize #Calculate data size
 
@@ -76,33 +98,37 @@ class EventsMenu(QDialog):
             QApplication.processEvents()    #Process events from the current call
 
     def Download(self): 
-  
-        url = 'https://speed.hetzner.de/100MB.bin' #URL to download from 
-
+        url = 'https://speed.hetzner.de/100MB.bin' #URL to download from
         filename = 'test.txt'
         path = 'C:/ECE 458/GUI/' + filename #Path to download to
         self.ToggleSpinner()
         urllib.request.urlretrieve(url, path, self.DownProgress)  #Download using urllib, call DownProgress
         self.ToggleSpinner()
 
-    
-
+#Class method to switch windows using the stacked widget
+class WindowSwitch():
+    def Switch(self, name):
+        if name == 'Main':
+            stack.setCurrentIndex(0)    #Main window index is 0
+        elif name == 'Options':
+            stack.setCurrentIndex(1)    #Options window index is 1
+        elif name == 'Events':
+            stack.setCurrentIndex(2)    #Events window index is 2
 
 #Main
 if __name__ == "__main__":
-    app = QApplication(sys.argv)        #Initialize GUI
-    widget = QtWidgets.QStackedWidget() #Create stacked widget to store windows in an array (0,1,2...)
-    mainwindow = MainWindow()           #Initialize mainwindow
-    widget.addWidget(mainwindow)        #Add mainwindow widget to widget stack
-    optionsmenu = OptionsMenu()         #Initialize optionsmenu
-    widget.addWidget(optionsmenu)       #Add optionsmenu widget to widget stack
-    eventsmenu = EventsMenu()           #Initialize eventsmenu
-    widget.addWidget(eventsmenu)        #Add eventsmenu widget to widget stack
-    widget.setFixedHeight(481)          #Set mainwindow height
-    widget.setFixedWidth(781)               #and width
-    widget.show()                       #Show the widget
+    app = QApplication(sys.argv)
+    stack = QtWidgets.QStackedWidget()  #Create stacked widget to hold all windows
+    home = MainWindow()
+    stack.addWidget(home)
+    options = Options()
+    stack.addWidget(options)
+    events = Events()
+    stack.addWidget(events)
+    stack.setMinimumSize(781, 481)
+    stack.show()
 
-
+#On program exit, print "Exiting" to console
 try:
     sys.exit(app.exec_())
 except:
