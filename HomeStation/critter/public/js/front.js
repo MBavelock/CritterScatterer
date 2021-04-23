3d59140d406b4221f545a9f8447e5baabd52ec85
acunningham3 <43209704+acunningham3@users.noreply.github.com>
2021-04-23 15:33:13 -0400

    Updated files to latest versions
    For real this time

--- HomeStation/critter/public/js/front.js
+++ HomeStation/critter/public/js/front.js
@@ -47,27 +47,111 @@ window.addEventListener('pageshow', function (event) {
   fader.setAttribute('class', 'fade-out')
 });
 
-/*
-Promise.all([
-  fetch('/Logs/4-12-2021_1400.txt').then(x => x.text())
-]).then(([sampleResp]) => {
-  console.log(sampleResp);
-});
-*/
-
 /*===================================*/
 /*    Log directory loop settings    */
 /*===================================*/
-let str = '<ul>'
 let fileNames = []
-var socket = io()
+let dirPath = '/critter/logs'
+var arr = {}
 
-socket.on('initialize array', function(init_fileNames) {
-  fileNames = init_fileNames
-})
-  .on('change', function(data) {
-    fileNames.push(data)
-    console.log(fileNames)
+//Formats for current date
+var q = new Date()
+var mm = q.getMonth() + 1
+var dd = q.getDate()
+var yyyy = q.getFullYear()
+if(mm < 10) {
+  mm = '0' + mm
+}
+if(dd < 10) {
+  dd = '0' + dd
+}
+var today = mm + '-' + dd + '-' + yyyy
+console.log('Todays date: ' + today)
+
+//AJAX call to server
+function ajaxCall(callback) {
+  $.ajax({
+    type: 'GET',
+    url: dirPath,
+    dataType: 'json',
+    success: callback
+  })
+}
+ajaxCall(result => {
+  console.log('Data received: ' + JSON.stringify(result))
+  //Store object in localstorage for addressing later
+  localStorage.setObject(result.Entry, )
+  //Call function to add to tab menus
+  DataEntry(result)
 })
 
-str += '</ul>'
+//Function takes in JSON data string and displays as log in tabs
+function DataEntry(data) {
+  //If log date is today, put in today panel
+  if (today == JSON.stringify(data.Date).replace(/['"]+/g, '')) {
+    var ul = document.getElementById('today-logs')
+    var li = document.createElement('li')
+    ul.appendChild(li)
+    li.innerHTML = li.innerHTML + data.Entry
+  }
+  //Else if log date is older than today, put in older panel
+  else if (today > JSON.stringify(data.Date).replace(/['"]+/g, '')) {
+    var ul = document.getElementById('older-logs')
+    var li = document.createElement('li')
+    ul.appendChild(li)
+    li.innerHTML = li.innerHTML + data.Entry
+  }
+  
+  //If a log file is clicked, then loop through JSON and display info
+  var ultoday = document.getElementById('today-logs')
+  ultoday.addEventListener('click', function(event) {
+    for (var key in data) {
+      if(key == 'Entry') {
+      }
+      //Convert time from millitary to default using moment
+      else if(key == 'Time') {
+        var p = document.getElementById(key)
+        p.innerHTML = moment(data.Time, 'HH::mm').format('h:mm A')
+        //Have text fade in from white
+        p.style.color = "#e65656";
+      }
+      else {
+        var p = document.getElementById(key)
+        p.innerHTML = data[key]
+        p.style.color = "#e65656";
+      }
+    }
+  })
+
+  //If a log file is clicked, then loop through JSON and display info
+  var ulolder = document.getElementById('older-logs')
+  ulolder.addEventListener('click', function(event) {
+    for (var key in data) {
+      if(key == 'Entry') {
+      }
+      //Convert time from millitary to default using moment
+      else if(key == 'Time') {
+        var p = document.getElementById(key)
+        p.innerHTML = moment(data.Time, 'HH::mm').format('h:mm A')
+        //Have text fade in from white
+        p.style.color = "#e65656";
+      }
+      else {
+        var p = document.getElementById(key)
+        p.innerHTML = data[key]
+        p.style.color = "#e65656";
+      }
+    }
+  })
+}
+
+//Set two functions to insert objects into browser localstorage
+//Using a key,value pair
+Storage.prototype.setObject = function(key, value) {
+  this.setItem(key, JSON.stringify(value));
+}
+
+Storage.prototype.getObject = function(key) {
+  var value = this.getItem(key);
+  return value && JSON.parse(value);
+}
